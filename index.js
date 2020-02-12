c56175a440c93c055c2b1f885949f4ea69bf4667
Carmine Zaccagnino <carmine@carminezacc.com>
2020-02-12 18:29:54 +0100

    Started logging meaningful stuff and not passwords
    Signed-off-by: Carmine Zaccagnino <carmine@carminezacc.com>

--- index.js
+++ index.js
@@ -13,27 +13,24 @@ var app = express();
 app.post('/signup', express.urlencoded(), function(req, res) {
   // in a production environment you would ideally add salt and store that in the database as well
   // or even use bcrypt instead of sha256. No need for external libs with sha256 though
-  console.log(req.body.password);
   var password = crypto.createHash('sha256').update(req.body.password).digest('hex');
-  console.log("Hashed password:<"+password+">");
   db.get("SELECT FROM users WHERE username = ?", [req.body.username], function(err, row) {
     if(row != undefined ) {
-      console.error("can't create user");
-      res.error(
-        "An user with that username already exists"
-      );
+      console.error("can't create user " + req.body.username);
+      res.status(409);
+      res.send("An user with that username already exists");
     } else {
-      console.log("Can create user");
+      console.log("Can create user " + req.body.username);
       db.run('INSERT INTO users(username, password) VALUES (?, ?)', [req.body.username, password]);
+      res.status(201);
       res.send("Success");
     }
   });
 });
 
 app.post('/login', express.urlencoded(), function(req, res) {
-  console.log(req.body.username);
+  console.log(req.body.username + " attempted login");
   var password = crypto.createHash('sha256').update(req.body.password).digest('hex');
-  console.log(password);
   db.get("SELECT * FROM users WHERE (username, password) = (?, ?)", [req.body.username, password], function(err, row) {
     if(row != undefined ) {
       var payload = {
@@ -41,9 +38,11 @@ app.post('/login', express.urlencoded(), function(req, res) {
       };
 
       var token = jwt.sign(payload, KEY, {algorithm: 'HS256', expiresIn: "15d"});
-
+      console.log("Success");
       res.send(token);
     } else {
+      console.error("Failure");
+      res.status(401)
       res.send("There's no user matching that");
     }
   });
